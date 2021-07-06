f3229738c42d9e008d75eea7b899740113792eb7
Cole O'brien <coobr@netcompany.com>
2021-07-06 21:48:36 +0100

    Initialise capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.iam.mobile',
+  appName: 'iota-address-monitor',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
