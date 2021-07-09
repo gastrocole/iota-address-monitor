ce26da9713cea375370d3e489cf25ab58290abed
Cole O'brien <coobr@netcompany.com>
2021-07-09 16:10:36 +0100

    Update formatting

--- src/utils/aysncTimeout.ts
+++ src/utils/aysncTimeout.ts
@@ -1,3 +1,3 @@
 export const asyncTimeout = async (duration) => {
-  await new Promise((resolve) => setTimeout(resolve, duration));
+	await new Promise((resolve) => setTimeout(resolve, duration));
 };
--- src/utils/convertBalance.ts
+++ src/utils/convertBalance.ts
@@ -1,25 +1,22 @@
-
 enum UNITS {
-  'i' = 0,
-  'Ki' = 1,
-  'Mi' = 2,
-  'Gi' = 3,
-  'Ti' = 4,
-  'Pi' = 5,
+	'i' = 0,
+	'Ki' = 1,
+	'Mi' = 2,
+	'Gi' = 3,
+	'Ti' = 4,
+	'Pi' = 5,
 }
 
 export const convertBalance = (balance: any, decimalFigures: number = 3) => {
-    const balanceString: string = String(balance);
-    const unitNumber = Math.floor((balanceString.length - 1) / 3);
-    const unit = UNITS[unitNumber];
+	const balanceString: string = String(balance);
+	const unitNumber = Math.floor((balanceString.length - 1) / 3);
+	const unit = UNITS[unitNumber];
 
-    const remainder = balanceString.length % 3;
+	const remainder = balanceString.length % 3;
 
-    const wholePart = balanceString.substring(0, remainder);
-    const decimalPart = balanceString.substring(remainder, remainder + decimalFigures);
-    const balanceAsDecimal = decimalPart.length > 0 ? wholePart + '.' + decimalPart : wholePart;
+	const wholePart = balanceString.substring(0, remainder);
+	const decimalPart = balanceString.substring(remainder, remainder + decimalFigures);
+	const balanceAsDecimal = decimalPart.length > 0 ? wholePart + '.' + decimalPart : wholePart;
 
-    
-    return balanceAsDecimal + ' ' + unit;
-    
-}
+	return balanceAsDecimal + ' ' + unit;
+};
